5aaed379101eba31d167475e849ec221266559e4
James Storey <storey.james@gmail.com>
2014-04-03 23:42:48 -0500

    row of cubes, added music file

--- Sunset.js
+++ Sunset.js
@@ -30,15 +30,20 @@ var Sunset = function () {
 	}
 
 	var dL = new THREE.DirectionalLight({color: 0xffffff});
+	var cubeGeo = new THREE.CubeGeometry(5, 5, 5, 10, 10, 10);
+	var mat = new THREE.MeshLambertMaterial({color: 0xffffff});
+	var c;
 	var time = 0;
 
 	var init = function () {
 		dL.position = new THREE.Vector3( 1, 0.5, 1 );
 		scene.add(dL);
 
-		var cubeGeo = new THREE.CubeGeometry(5, 5, 5, 10, 10, 10);
-		var mat = new THREE.MeshLambertMaterial({color: 0xffffff});
-		var c = new THREE.Mesh(cubeGeo, mat);
+		c = new THREE.Object3D();
+		for (var i = 0; i < 100; i += 1) {
+			c.add(new THREE.Mesh(cubeGeo, mat).translateZ((i * 20) - 1000));
+		}
+		
 		c.translateX(-20);
 		c.translateY(2.5);
 		scene.add(c);
@@ -51,7 +56,7 @@ var Sunset = function () {
 		requestAnimationFrame (update);
 		time += 0.01;
 		dL.position = new THREE.Vector3( Math.cos(time), 1.5, Math.sin(time) );
-
+		c.position = new THREE.Vector3( c.position.x, c.position.y, (time % 5) * -200);
 		renderer.render(scene, camera);
 	};
 	
